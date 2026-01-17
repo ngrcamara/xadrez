e0ef56b647b1b69dd9544e1f3021fe9743ac0a4a
ngrcamara <nereucamara@icloud.com>
2026-01-17 13:48:25 -0300

    Create xadrez_novato.c

---
+++ xadrez_novato.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+
+    const int LIMIT_MOV_TORRE = 5;
+    const int LIMIT_MOV_BISPO = 5;
+    const int LIMIT_MOV_RAINHA = 8;
+
+    //movimento da torre...
+    printf("Movimento da torre...\n");
+    int cont_torre = 1;
+    while (cont_torre <= LIMIT_MOV_TORRE) {
+        printf("Direita.\n");
+        cont_torre++;
+    }
+
+    printf("\nMovimento do Bispo...\n");
+    int cont_bispo = 1;
+    do {         
+        printf("Cima, Direita.\n");
+        cont_bispo++;
+    } while (cont_bispo <= LIMIT_MOV_BISPO);
+
+    printf("\nMovimento da Rainha...\n");
+    for (int cont_rainha = 1; cont_rainha <= LIMIT_MOV_RAINHA; cont_rainha++) {
+        printf("Esquerda.\n");
+    }
+}
